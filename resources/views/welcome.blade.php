9e0b5576c0ded00cd524457b293a5d6b815f88fd
aspsptyd <devadiroot@gmail.com>
2024-12-10 21:28:40 +0700

    Fix: Menambahkan pengecekan khusus engineer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,8 +35,8 @@
                                         href="
                                             @if (\Auth::user()->role_access === 'teamlead')
                                                 {{ route('teamlead.dashboard'); }}
-                                            @else
-                                                
+                                            @elseif (\Auth::user()->role_access === 'engineer')
+                                                {{ route('engineer.dashboard'); }}
                                             @endif
                                         "
                                         class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
